ff5ce2b041bcae6b83bfdfbb9a040eeaa3dcdd0d
Pratik Patil <cyclopes82@gmail.com>
2023-04-25 18:30:26 +0530

    section17 - Functions

---
+++ first_function.py
@@ -0,0 +1,8 @@
+def sing_happy_birthday(name):
+    print("Happy Birthday To You")
+    print("Happy Birthday To You")
+    print(f"Happy Birthday Dear {name}")
+    print("Happy Birthday To You")
+
+sing_happy_birthday("Chhotu") #parenthesis is mandatory 
+sing_happy_birthday("Motu")
---
+++ return.py
@@ -0,0 +1,14 @@
+def print_square_of_7():
+    print(7**2)
+
+print_square_of_7()
+
+
+def square_of_7():
+    return 7**2
+#return exits the function. 
+
+
+result = square_of_7()
+
+print(result)
